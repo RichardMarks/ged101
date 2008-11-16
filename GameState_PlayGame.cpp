9f7e5d65739b9053e92c42875f39aee72eea61ce
Richard Marks <ccpsceo@gmail.com>
2008-11-15 23:31:43 -0600

    added missing CommunicationDevice class stubs so the project will build

--- GameState_PlayGame.cpp
+++ GameState_PlayGame.cpp
@@ -51,23 +51,151 @@ namespace GAME
 		// for our first call, since the timer handles the time calculations.
 		ENGINE::GameTimer;
 		
+		
+#if 1
+		// create a player sprite
+		ENGINE::GameSpriteManager->CreateSprite("player");
+		
+		// get a pointer to the player sprite
+		ENGINE::GameSprite* playerSprite = ENGINE::GameSpriteManager->GetSprite("player");
+		
+		// create an animation sequence for when the player is idle
+		playerSprite->CreateAnimationSequence("idle");
+		
+		// get a pointer to the idle animation sequence
+		ENGINE::AnimationSequence* playerSpriteIdleAnimSeq = playerSprite->GetAnimationSequence("idle");
+		
+		// set the animation frames for the idle animation sequence
+		playerSpriteIdleAnimSeq->SetFrames(
+			ENGINE::AnimationFrame("playersheet.png",   0,   0,  31,  31),
+			ENGINE::AnimationFrame("playersheet.png",  32,   0,  63,  31),
+			ENGINE::AnimationFrame("playersheet.png",  64,   0,  95,  31));
+		
+		// set the animation sequence frame playback mode to alternate
 		/*
-		
-		ENGINE::GameResource* music = ENGINE::GameResource::CreateAudioResource();
-		music->Load("music.ogg");
-		music->Play(true); // Play(bool playLooped = false);
-		music->Stop();
-		ENGINE::GameResource::DestroyAudioResource(music);
-		
-		
-		ENGINE::AudioEngine->Initialize();
-		unsigned int musicID = ENGINE::AudioEngine->Load("music.ogg");
-		ENGINE::AudioEngine->Play(musicID, true);
-		ENGINE::AudioEngine->Stop(musicID);
-		ENGINE::AudioEngine->Destroy(musicID);
-		
-		
+			Playback Modes:
+			
+			ANIMATION_SEQ_PLAYBACK_ALTERNATE
+			
+				When the last frame is reached, the animation plays in reverse until
+				the first frame is reached, and the the animation plays forwards again.
+				This repeats indefinitely.
+				
+			ANIMATION_SEQ_PLAYBACK_REPEAT
+			
+				The animation starts back at the first frame when the last frame is reached.
+				
+			ANIMATION_SEQ_PLAYBACK_ALTERNATE_REVERSE
+			
+				Same as ANIMATION_SEQ_PLAYBACK_ALTERNATE except the animation starts on the
+				last frame, and plays towards the first frame.
+				
+			ANIMATION_SEQ_PLAYBACK_REPEAT_REVERSE
+			
+				Same as ANIMATION_SEQ_PLAYBACK_REPEAT except the animation starts on the
+				last frame, and plays towards the first frame.
+				
+			ANIMATION_SEQ_PLAYBACK_RANDOM
+			
+				A random frame is selected each time a frame change is required.
+				
+			ANIMATION_SEQ_PLAYBACK_CUSTOM
+			
+				You can specify a complicated arrangement of your frames by first
+				setting this mode, and then calling SetCustomSequence() with an
+				array of signed ints. Negative values correspond to the last frame
+				plus one, minus the absolute value of the negative value.
+				So -1 means the last frame.
 		*/
+		playerSpriteIdleAnimSeq->SetPlaybackMode(ENGINE::ANIMATION_SEQ_PLAYBACK_ALTERNATE);
+		
+		// set the delay times in milliseconds for each frame in the idle animation sequence
+		playerSpriteIdleAnimSeq->SetGlobalDelay(500); // half a second should pass before frame changes
+		
+		
+		
+		
+		// create a complex animation sequence
+		
+		// create an animation sequence for when the player is attacking with a sword
+		playerSprite->CreateAnimationSequence("attack with sword");
+		
+		// get a pointer to the animation sequence
+		ENGINE::AnimationSequence* playerSpriteSwordAttackAnimSeq = playerSprite->GetAnimationSequence("attack with sword");
+		
+		// We will place the rotation of the sword into its own sequence.
+		ENGINE::AnimationSequence* sword360AnimSeq = new ENGINE::AnimationSequence();
+		
+		// set the delay times in milliseconds for each frame in the sword rotation animation sequence
+		sword360AnimSeq->SetGlobalDelay(250); // a quarter second should pass before frame changes
+		
+		// set the animation sequence frame playback mode to repeat
+		sword360AnimSeq->SetPlaybackMode(ENGINE::ANIMATION_SEQ_PLAYBACK_REPEAT);
+		
+		// load the image resource for the sword
+		ENGINE::ImageResource* swordImage = new ENGINE::ImageResource();
+		swordImage->LoadFrom("weaponsheet.png", 0, 0, 8, 16);
+		
+		// add the 8 rotation frames
+		/*
+			the sword is rotation 45 degrees each frame
+			
+			the position is relative to the anchor point
+			
+			-4 -3 -2 -1  0  1  2  3  4
+			[ ][ ][ ][ ][1][ ][ ][ ][ ] -4
+			[ ][ ][ ][ ][ ][ ][ ][ ][ ] -3
+			[ ][ ][8][ ][ ][ ][2][ ][ ] -2
+			[ ][ ][ ][ ][ ][ ][ ][ ][ ] -1
+			[7][ ][ ][ ][X][ ][ ][ ][3]  0
+			[ ][ ][ ][ ][ ][ ][ ][ ][ ]  1
+			[ ][ ][6][ ][ ][ ][4][ ][ ]  2
+			[ ][ ][ ][ ][ ][ ][ ][ ][ ]  3
+			[ ][ ][ ][ ][5][ ][ ][ ][ ]  4
+			
+			The X is the anchor point, and the number 1 through 8 in the [ ] are the frames.
+		*/
+		
+		// void AnimationSequence::AddNamedFrameEx(const char* name, ImageResource* image, int relativeX, int relativeY, int rotation);
+		sword360AnimSeq->AddNamedFrameEx("sword n",  swordImage,   0,  -4,  0);
+		sword360AnimSeq->AddNamedFrameEx("sword ne", swordImage,   2,  -2,  45);
+		sword360AnimSeq->AddNamedFrameEx("sword e",  swordImage,   4,   0,  90);
+		sword360AnimSeq->AddNamedFrameEx("sword se", swordImage,   2,   2,  135);
+		sword360AnimSeq->AddNamedFrameEx("sword s",  swordImage,   0,   4,  180);
+		sword360AnimSeq->AddNamedFrameEx("sword sw", swordImage,  -2,   2,  225);
+		sword360AnimSeq->AddNamedFrameEx("sword w",  swordImage,  -4,   0,  270);
+		sword360AnimSeq->AddNamedFrameEx("sword nw", swordImage,  -2,  -2,  315);
+		
+		// set the drawing anchor of the sword sequence
+		playerSpriteSwordAttackAnimSeq->SetAnchorPoint(4, 14);
+		
+		// attach the sword animation sequence to the player's attack animation sequence 
+		// to start at frame 1 of the parent animation
+		playerSpriteSwordAttackAnimSeq->AddSequenceLayer("sword 360", sword360AnimSeq, 1);
+		
+		// set the animation frames for the player's attack animation sequence
+		playerSpriteSwordAttackAnimSeq->AddNamedFrame("face n",  ENGINE::AnimationFrame("playersheet.png",   0,   31,  31,  63));
+		playerSpriteSwordAttackAnimSeq->AddNamedFrame("face ne", ENGINE::AnimationFrame("playersheet.png",  32,   31,  63,  63));
+		playerSpriteSwordAttackAnimSeq->AddNamedFrame("face e",  ENGINE::AnimationFrame("playersheet.png",  64,   31,  95,  63));
+		playerSpriteSwordAttackAnimSeq->AddNamedFrame("face se", ENGINE::AnimationFrame("playersheet.png",  96,   31, 127,  63));
+		playerSpriteSwordAttackAnimSeq->AddNamedFrame("face s",  ENGINE::AnimationFrame("playersheet.png", 128,   31, 159,  63));
+		playerSpriteSwordAttackAnimSeq->AddNamedFrame("face sw", ENGINE::AnimationFrame("playersheet.png", 160,   31, 191,  63));
+		playerSpriteSwordAttackAnimSeq->AddNamedFrame("face w",  ENGINE::AnimationFrame("playersheet.png", 192,   31, 223,  63));
+		playerSpriteSwordAttackAnimSeq->AddNamedFrame("face nw", ENGINE::AnimationFrame("playersheet.png", 224,   31, 255,  63));
+		
+		// set the delay times in milliseconds for each frame in the player's attack animation sequence
+		playerSpriteSwordAttackAnimSeq->SetGlobalDelay(250); // a quarter second should pass before frame changes
+		
+		// set the animation sequence frame playback mode to repeat
+		playerSpriteSwordAttackAnimSeq->SetPlaybackMode(ENGINE::ANIMATION_SEQ_PLAYBACK_REPEAT);
+		
+		// set the drawing anchor point on the player sprite
+		playerSprite->SetAnchorPoint(15, 28); // between the player's legs and just above the player's ankles
+		
+		// select the animation sequence we want to use
+		playerSprite->SelectAnimationSequence("idle");
+		
+#endif
 	}
 	
 	/**************************************************************************/
