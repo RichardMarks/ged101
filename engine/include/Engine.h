96060e3c9d1d3aa075481042e74b011444182e46
Richard Marks <ccpsceo@gmail.com>
2008-11-18 14:50:42 -0600

    beginnings of proper documentation started added Doxyfile for using doxygen to generate docs added Engine.h which serves two purposes; the first is to include all the ged101 headers and the other is to provide a single location for the mainpage and namespace documentation

---
+++ engine/include/Engine.h
@@ -0,0 +1,60 @@
+
+// CODESTYLE: v2.0
+
+// Engine.h
+// Project: Game Engine Design 101 (ENGINE)
+// Author: Richard Marks
+
+#ifndef __ENGINE_H__
+#define __ENGINE_H__
+#if 0
+// add any documentation to be placed on the index page of the generated documentation here
+#endif
+/**
+ * \mainpage
+ * The Game Engine Design 101 Project. (ged101)\n\n
+ * A collection of reusable classes that when used properly provide a cross-platform library of functionality to
+ * implement just about any game genre you can imagine... within limits of course.\n
+ * ged101 is purely 2D at this time.
+ */
+
+#if 0
+// add any documentation for the namespaces here
+#endif
+/**
+ * \namespace ALBASE
+ * \brief Initializes Allegro and handles setting up the main game instance.
+ */
+
+/** 
+ * \namespace DEBUG
+ * \brief Project Debugging Utility Library - Reusable error reporting classes and constants.
+ */
+
+/**
+ * \namespace ENGINE
+ * \brief The entire library of classes and constants that make up the ged101 engine.
+ */
+
+/**
+ * \namespace GAME
+ * \brief All the classes that make up your game. You are responsible for implementing all of this.
+ */
+
+
+
+
+// include the ged101 headers
+#include "DebugReport.h"
+#include "InputManager.h"
+#include "CommunicationDevice.h"
+#include "Audio.h"
+#include "ImageResource.h"
+#include "GameTimer.h"
+#include "GameObject.h"
+#include "GameObjectGroup.h"
+#include "GameObjectGroupManager.h"
+#include "GameStateManager.h"
+
+#endif
+
